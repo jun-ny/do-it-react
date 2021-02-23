446187c17a0f1f7f2bee14ce9701bc4540811b7d
junhyeong <whitelaw7@naver.com>
2021-02-24 00:45:31 +0900

    210224

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,12 @@
 import { configure } from '@storybook/react';
+import interopRequireDefault from 'babel-runtime/helpers/interopRequireDefault';
+
 
 function loadStories() {
-    require('../src/stories/InputStory');
-    //스토리 파일을 이곳에 추가할 수 있습니다.
+    const context = require.context('../src/stories', true, /Story\.jsx$/);
+    context.keys().forEach((srcFile) => {
+        interopRequireDefault(context(srcFile));
+    });
 }
 
 configure(loadStories, module);
