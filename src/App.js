59d03ff4540374c3edaf17ae5feae92383d0aaca
junhyeong <whitelaw7@naver.com>
2021-02-24 23:07:56 +0900

    210224

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import PropsComponent from './03/PropsComponent';
 
-class App extends React.Component {
+class App extends Component {
   render() {
   return (
-    <PropsComponent 
-    name="두잇 리액트"
-    />
+    <div>
+    <nav>
+    <div className="nav-wrapper">
+    <div>두잇! 리액트 시작하기</div>
+    </div>
+    </nav>
+    <h1>머티리얼 CSS</h1>
+    </div>
   ); 
 }
 }
